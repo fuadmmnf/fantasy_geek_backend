a815c7d299ae40a71fc51b1a0f37f847cd869ad2
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-10-30 23:02:58 +0600

    fixed team related api

--- app/Repositories/TeamRepository.php
+++ app/Repositories/TeamRepository.php
@@ -15,8 +15,6 @@ class TeamRepository
 
     function getTeam($team_id) {
         $team = Team::findOrfail($team_id);
-        $team->team_members = json_decode($team->team_members, true);
-        $team->key_members = json_decode($team->team_members, true);
 
         for ($i = 0; $i < count($team->team_members); $i++) {
             $player = Player::findOrFail($team->team_members[$i]['id']);
