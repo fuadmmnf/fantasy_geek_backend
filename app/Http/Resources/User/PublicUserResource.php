a3169201e386c814af598f5e3c55b7c399d0d157
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-12-16 18:31:50 +0600

    added user id to ranking response

--- app/Http/Resources/User/PublicUserResource.php
+++ app/Http/Resources/User/PublicUserResource.php
@@ -16,6 +16,7 @@ class PublicUserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'image' => $this->image
         ];
